6fc95dd9d82cf09c90fd575c0d6c020ed5dc4aa5
topdeveloper0729 <topdev0729@gmail.com>
2020-09-01 18:47:47 +0200

    Create first reducer, add thunk,Provider; start notes

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,12 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { productListReducer } from "./reducers/productReducers";
 
-const initialState = {}
+const initialState = {};
 const reducer = combineReducers({
   productList: productListReducer,
-})
+});
 
-const store = createStore(reducer, initialState);
+const store = createStore(reducer, initialState, compose(applyMiddleware(thunk)));
+
+export default store;
