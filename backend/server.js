0fcdfd5332addb17324a4fcb0b1bea7af577d0fc
topdeveloper0729 <topdev0729@gmail.com>
2020-09-01 18:45:43 +0200

    Clean up code

--- backend/server.js
+++ backend/server.js
@@ -7,4 +7,6 @@ app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
-app.listen(5000, () => {console.log("Server started at http://localhost:5000")})
+app.listen(5000, () => {
+  console.log("Server started at http://localhost:5000");
+});
