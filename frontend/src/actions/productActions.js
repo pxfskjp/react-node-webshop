4c574eeb798954dd86fa1441f69a223843fe4905
topdeveloper0729 <topdev0729@gmail.com>
2020-09-02 17:32:27 +0200

    Create productsId api, constants and use product details in productScreen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,10 +1,12 @@
 // Each dispatch has a type and a payload
 import axios from "axios";
-
 import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from "../constants/productConstants";
 
 const listProducts = () => async (dispatch) => {
@@ -17,4 +19,14 @@ const listProducts = () => async (dispatch) => {
   }
 };
 
-export { listProducts }
+const detailsProduct = (productId) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productId })
+    const { data } = await axios.get("/api/products/" + productId)
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data })
+  } catch (error) {
+    dispatch({ type: PRODUCT_DETAILS_FAIL, payload: error.message })
+  }
+}
+
+export { listProducts, detailsProduct }
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,56 +1,71 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import data from "../data";
+import { useSelector, useDispatch } from "react-redux";
+import { detailsProduct } from "../actions/productActions";
 
 function ProductScreen(props) {
-  console.log(props.match.params.id);
+  const productDetails = useSelector((state) => state.productDetails);
+  const { product, loading, error } = productDetails;
+  const dispatch = useDispatch();
 
-  const product = data.products.find((x) => x._id == props.match.params.id);
+  useEffect(() => {
+    dispatch(detailsProduct());
+    return () => {
+      //
+    };
+  }, []);
 
   return (
     <div>
       <div className="back-to-result">
         <Link to="/">Back to result</Link>
       </div>
-      <div className="details">
-        <div className="details-image">
-          <img src={product.image} alt="product" />
+      {loading ? (
+        <div>Loading ...</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
+        <div className="details">
+          <div className="details-image">
+            <img src={product.image} alt="product" />
+          </div>
+          <div className="details-info">
+            <ul>
+              <li>
+                <h4>{product.name}</h4>
+              </li>
+              <li>
+                {product.rating} Stars ({product.numReviews} Reviews)
+              </li>
+              <li>
+                Price: <b>${product.price}</b>
+              </li>
+              <li>
+                Description:
+                <div>{product.description}</div>
+              </li>
+            </ul>
+          </div>
+          <div className="details-action">
+            <ul>
+              <li>Price: ${product.price}</li>
+              <li>Status: {product.status}</li>
+              <li>
+                Quantity:{" "}
+                <select>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                </select>
+              </li>
+              <li>
+                <button className="button">Add to cart</button>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="details-info">
-          <ul>
-            <li>
-              <h4>{product.name}</h4>
-            </li>
-            <li>
-              {product.rating} Stars ({product.numReviews} Reviews)
-            </li>
-            <li>
-              Price: <b>${product.price}</b>
-            </li>
-            <li>
-              Description:
-              <div>
-                {product.description}
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div className="details-action">
-          <ul>
-            <li>Price: ${product.price}</li>
-            <li>Status: {product.status}</li>
-            <li>Quantity: <select>
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-            </select></li>
-            <li>
-              <button className="button">Add to cart</button>
-            </li>
-          </ul>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
