2414bfcd01f5734e17ceb4aabebed67f6e5b793e
topdeveloper0729 <topdev0729@gmail.com>
2020-09-04 18:32:28 +0200

    Fix productscreen error; add data ids

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { detailsProduct } from "../actions/productActions";
