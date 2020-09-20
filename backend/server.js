de3b74152414f2bebcf0c0635d450906f7f7591d
topdeveloper0729 <topdev0729@gmail.com>
2020-09-20 17:48:50 +0200

    Create .env file, install dotenv, configure mongodbUrl, connect mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,15 @@
 import express from "express";
 import data from "./data";
+import dotenv from "dotenv"
+import config from "./config"
+import mongoose from "mongoose"
+
+dotenv.config()
+
+const mongodbUrl = config.MONGODB_URL
+mongoose.connect(mongodbUrl, {
+  useNewUrlParser: true
+}).catch(error => console.log(error.reason))
 
 const app = express();
 
